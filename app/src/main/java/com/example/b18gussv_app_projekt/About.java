62966acddac36491981ae3f483cd3767eee3f03f
b18gussv <b18gussv@student.his.se>
2019-05-15 10:09:45 +0200

    Ändrat teman på About för att skilja den från Main. Har således även städat upp layoutfiler för både Main och About.

--- app/src/main/java/com/example/b18gussv_app_projekt/About.java
+++ app/src/main/java/com/example/b18gussv_app_projekt/About.java
@@ -1,29 +1,30 @@
 package com.example.b18gussv_app_projekt;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.support.annotation.NonNull;
 import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.NavigationView;
 import android.support.design.widget.Snackbar;
+import android.view.View;
+import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
-import android.widget.TextView;
+import android.content.Intent;
+import android.support.annotation.NonNull;
 
-public class About extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
+public class About extends AppCompatActivity
+        implements NavigationView.OnNavigationItemSelectedListener {
+    public static String EXTRA_MESSAGE = "DONK";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_about);
-        Toolbar toolbar = findViewById(R.id.toolbar);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-/*
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
@@ -32,31 +33,58 @@ public class About extends AppCompatActivity implements NavigationView.OnNavigat
         toggle.syncState();
 
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
-*/
-        Intent intent = getIntent();
-      //  String message = intent.getStringExtra(MainActivity.EXTRA_MESSAGE);
+        navigationView.setNavigationItemSelectedListener(this);
     }
 
-        //TextView textView = findViewById(R.id.text_info);
-        @SuppressWarnings("StatementWithEmptyBody")
-        @Override
-        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-            // Handle navigation view item clicks here.
-            int id = item.getItemId();
-
-            if (id == R.id.about) {
-                Intent i = new Intent(About.this, About.class);
-                startActivity(i);
-            } else if(id == R.id.list){
-                Intent a = new Intent(About.this, MainActivity.class);
-                startActivity(a);
-            }
-
-
-            DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+    @Override
+    public void onBackPressed() {
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        if (drawer.isDrawerOpen(GravityCompat.START)) {
             drawer.closeDrawer(GravityCompat.START);
-            return true;
+        } else {
+            super.onBackPressed();
         }
     }
 
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
 
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    @SuppressWarnings("StatementWithEmptyBody")
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        // Handle navigation view item clicks here.
+        int id = item.getItemId();
+
+        if (id == R.id.about) {
+            Intent i = new Intent(About.this, About.class);
+            startActivity(i);
+        } else if(id == R.id.list){
+            Intent a = new Intent(About.this, MainActivity.class);
+            startActivity(a);
+        }
+
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
+    }
+}
