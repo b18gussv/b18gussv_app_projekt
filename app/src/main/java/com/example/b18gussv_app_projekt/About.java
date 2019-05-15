81e723324becfe2b4330019830dab525f4028ab4
b18gussv <b18gussv@student.his.se>
2019-05-15 09:29:13 +0200

    Kan nu byta mellan activities med hjälp av navigationsmenyn.

--- app/src/main/java/com/example/b18gussv_app_projekt/About.java
+++ app/src/main/java/com/example/b18gussv_app_projekt/About.java
@@ -2,17 +2,20 @@ package com.example.b18gussv_app_projekt;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.NavigationView;
 import android.support.design.widget.Snackbar;
+import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.TextView;
 
-public class About extends AppCompatActivity  {
+public class About extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,15 +23,7 @@ public class About extends AppCompatActivity  {
         setContentView(R.layout.activity_about);
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+/*
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
@@ -37,13 +32,31 @@ public class About extends AppCompatActivity  {
         toggle.syncState();
 
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
+*/
+        Intent intent = getIntent();
+      //  String message = intent.getStringExtra(MainActivity.EXTRA_MESSAGE);
     }
 
-        Intent intent = getIntent();
-        String message = intent.getStringExtra(MainActivity.EXTRA_MESSAGE);
+        //TextView textView = findViewById(R.id.text_info);
+        @SuppressWarnings("StatementWithEmptyBody")
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+            // Handle navigation view item clicks here.
+            int id = item.getItemId();
 
-        TextView textView = findViewById(R.id.text_info);
+            if (id == R.id.about) {
+                Intent i = new Intent(About.this, About.class);
+                startActivity(i);
+            } else if(id == R.id.list){
+                Intent a = new Intent(About.this, MainActivity.class);
+                startActivity(a);
+            }
 
+
+            DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+            drawer.closeDrawer(GravityCompat.START);
+            return true;
+        }
     }
 
 
