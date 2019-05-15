27043a15a5d74e37d7310be1cdf6462108f15273
b18gussv <b18gussv@student.his.se>
2019-05-15 11:37:21 +0200

    Importerar data genom JSON och lägger det i en lista som visas upp i Main. Listan innehåller även en Toast med mer detaljerad information om varje objekt.

--- app/src/main/java/com/example/b18gussv_app_projekt/MainActivity.java
+++ app/src/main/java/com/example/b18gussv_app_projekt/MainActivity.java
@@ -1,8 +1,10 @@
 package com.example.b18gussv_app_projekt;
 
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.util.Log;
 import android.view.View;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
@@ -14,9 +16,26 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.content.Intent;
 import android.support.annotation.NonNull;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
+    private ArrayList<Player> playerArrayList=new ArrayList<>();
     public static String EXTRA_MESSAGE = "DONK";
 
     @Override
@@ -25,6 +44,17 @@ public class MainActivity extends AppCompatActivity
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+        ArrayAdapter<Player> adapter=new ArrayAdapter<>(this,R.layout.list_item_textview,R.id.list_item_textview,playerArrayList);
+        final ListView my_listview=(ListView) findViewById(R.id.list_item_textview);
+        my_listview.setAdapter(adapter);
+        my_listview.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Toast.makeText(getApplicationContext(), playerArrayList.get(position).info(), Toast.LENGTH_LONG).show();
+            }
+        });
+        new FetchData().execute();
+
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
@@ -48,19 +78,13 @@ public class MainActivity extends AppCompatActivity
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.main, menu);
         return true;
     }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
         if (id == R.id.action_settings) {
             return true;
         }
@@ -71,7 +95,6 @@ public class MainActivity extends AppCompatActivity
     @SuppressWarnings("StatementWithEmptyBody")
     @Override
     public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-        // Handle navigation view item clicks here.
         int id = item.getItemId();
 
         if (id == R.id.about) {
@@ -86,5 +109,83 @@ public class MainActivity extends AppCompatActivity
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         drawer.closeDrawer(GravityCompat.START);
         return true;
+    }
+    private class FetchData extends AsyncTask<Void,Void,String> {
+        @Override
+        protected String doInBackground(Void... params) {
+            HttpURLConnection urlConnection = null;
+            BufferedReader reader = null;
+            String jsonStr = null;
+
+            try {
+                URL url = new URL("http://wwwlab.iit.his.se/b18gussv/Dink/json/player.json");
+                urlConnection = (HttpURLConnection) url.openConnection();
+                urlConnection.setRequestMethod("GET");
+                urlConnection.connect();
+                InputStream inputStream = urlConnection.getInputStream();
+                StringBuffer buffer = new StringBuffer();
+                if (inputStream == null) {
+                    // Nothing to do.
+                    return null;
+                }
+                reader = new BufferedReader(new InputStreamReader(inputStream));
+
+                String line;
+                while ((line = reader.readLine()) != null) {
+                    buffer.append(line + "\n");
+                }
+
+                if (buffer.length() == 0) {
+                    return null;
+                }
+                jsonStr = buffer.toString();
+                return jsonStr;
+            } catch (IOException e) {
+                Log.e("PlaceholderFragment", "Error ", e);
+                return null;
+            } finally{
+                if (urlConnection != null) {
+                    urlConnection.disconnect();
+                }
+                if (reader != null) {
+                    try {
+                        reader.close();
+                    } catch (final IOException e) {
+                        Log.e("Network error", "Error closing stream", e);
+                    }
+                }
+            }
+        }
+        @Override
+        protected void onPostExecute(String o) {
+            super.onPostExecute(o);
+            Log.d("Player",o);
+
+            try{
+                JSONArray parray = new JSONArray(o);
+                Log.d("Player",parray.get(0).toString());
+
+                for (int i = 0; i < parray.length();i++){
+                    JSONObject player = parray.getJSONObject(i);
+                    String name = player.getString("name");
+                    int height = player.getInt("size");
+                    String location = player.getString("location");
+                    String company = player.getString("company");
+                    String category = player.getString("category");
+                    int cost = player.getInt("cost");
+                    playerArrayList.add(new Player(name,location,height,company,category,cost));
+
+                }
+                ArrayAdapter<Player> adapter=new ArrayAdapter<Player>(getApplicationContext(),R.layout.list_item_textview,R.id.list_item_textview,playerArrayList);
+                final ListView my_listview=(ListView) findViewById(R.id.list_item_textview);
+                my_listview.setAdapter(adapter);
+            }catch (JSONException e){
+                Log.e("donk","E:"+e.getMessage());
+            }
+
+
+
+        }
+
     }
 }
